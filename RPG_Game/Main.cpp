0c5970362465e4fcdfecaf6e2af92e28fc8b9d3f
Moeez Abdul <moeezrajput2000@gmail.com>
2022-12-08 13:04:08 +0000

    Game_RPG - V0.2 Description: 1) Added Includes 2) Created Required Functions 3) Created Global Variables for Charachter and Monster

---
+++ RPG_Game/Main.cpp
@@ -0,0 +1,43 @@
+// Includes located below:
+#include <iostream>
+#include <windows.h>
+#include <string>
+
+// Using Directive
+using namespace std;
+
+// Function HUD
+void HUD();
+
+// Function Combat
+void Combat();
+
+// Function CombatHUD
+void CombatHUD();
+
+// Function Moving
+void Moving();
+
+// Function CreateMonster
+void CreateMonster();
+
+// Global Variables for Characheter Below;
+string CharachterName = " ";
+string CharachterType = " ";
+string CharachterGender = " ";
+
+int CharachterLevel = 0;
+int CharachterXP = 0;
+int CharachterHealth = 0;
+int CharachterMaxHealth = 0;
+int CharachterLevelUP = 0;
+int CharachterHeal = 0;
+
+// Global Variables for Monster Below;
+int MonsterHP = 0;
+int MonsterXP = 0;
+int MonsterLevel = 0;
+// Main Function
+int main() {
+	
+}
